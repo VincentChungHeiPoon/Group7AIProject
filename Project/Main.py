240145a647eb94290a1f51d5e072ce46e3cb0179
Vincent Poon <oy19970722@hotmail.com>
2019-03-14 22:34:27 -0500

    imported Agent class from agent.py

--- Project/Main.py
+++ Project/Main.py
@@ -1,1 +1,9 @@
+#if you have a class you created, use the syntax from "file" import "class"
+#aware that file do not have .py extension anymore
+from agent import Agent
 print("Hello World")
+
+
+a1 = Agent(0,0,True)
+
+print(a1.havePackage)
