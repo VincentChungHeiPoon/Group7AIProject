8b5cdfc868917bba7d6abea6fd34246668e55eaf
Logan Fortune <lfortune04@gmail.com>
2019-04-01 17:53:50 -0500

    Add files via upload
    Storing has been changed. Movement is ready to be tested

--- Project/Storing.py
+++ Project/Storing.py
@@ -2,86 +2,54 @@
 from agent import Agent
 from random import randint
 
-#Ignore this using it for testing
-w = 5
-h = 5
-d = 4
-QMatrix = [[[0 for x in range(d)] for y in range(w)] for z in range(h)]
 
-for x in range(0, 5):
-    for y in range(0,5):
-        for z in range(0,4):
-            act = "";
-            if z == 0:
-                act = "North"
-            if z == 1:
-                act = "East"
-            if z == 2:
-                act = "South"
-            if z == 3:
-                act = "West"
+class updateMatrix:
 
-            QMatrix[x][y][z] = 0
-
-            print("Position = [", x, ", ", y, "], Direction = ", act, ", Value = ", QMatrix[x][y][z])
-        print()
+    def moveDirection(oldAgent, newAgent) -> int:
+        # 0,1,2,3,4 for North, East, South, West, stayput(pickup/dropoff)
+        # if stayput
+        if (newAgent.x == oldAgent.x and newAgent.y == oldAgent.y):
+            return 4
+        # moved north
+        elif (newAgent.y < oldAgent.y and newAgent.x == oldAgent.x):
+            return 0
+        # moved East
+        elif (newAgent.x > oldAgent.x and newAgent.y == oldAgent.y):
+            return 1
+        # moved south
+        elif (newAgent.y > oldAgent.y and newAgent.x == oldAgent.x):
+            return 2
+        # moved West
+        elif (newAgent.x < oldAgent.x and newAgent.y == oldAgent.y):
+            return 3
 
 
-#Updating using Q-Learing
-def Qpickupordropoff(i, j, q, newi, newj, alpha, gamma):
-    QMatrix[i][j][q] = (1 - alpha) * (13-1) + gamma * max(QMatrix[newi][newj][0], QMatrix[newi][newj][1],
-                                                      QMatrix[newi][newj][2], QMatrix[newi][newj][3]);
-def Qmovespace(i, j, q, newi, newj, alpha, gamma):
-    QMatrix[i][j][q] = (1 - alpha) * (-1) + gamma * max(QMatrix[newi][newj][0], QMatrix[newi][newj][1],
-                                                      QMatrix[newi][newj][2], QMatrix[newi][newj][3]);
+    #Updating using Q-Learing
+    def Qpickupordropoff(agentOld, agentCurr, world, alpha, gamma):
+        reward = agentCurr.score - agentOld.score
 
-#Updating using SARSA
-def SARSApickupordropoff(i, j, q, newi, newj, alpha, gamma):
-    QMatrix[i][j][q] = QMatrix[i][j][q] + alpha(* (13-1) + (gamma*QMatrix[newi][newj][q] - QMatrix[i][j][q]));
+        oldX = agentOld.x
+        oldY = agentOld.y
+        newX = agentCurr.x
+        newY = agentCurr.y
 
-def SARSAmovespace(i, j, q, newi, newj, alpha, gamma):
-    QMatrix[i][j][q] = QMatrix[i][j][q] + alpha(*(-1) + (gamma * QMatrix[newi][newj][q] - QMatrix[i][j][q]));
+        nextBestQ = max(world.map[newX][newY].qNorth, world.map[newX][newY].qEast, world.map[newX][newY].qSouth, world.map[newX][newY].qEast)
 
-def QUpdate(agent):
-    #Receive Q-Matrix
+        movement = updateMatrix.moveDirection(agentOld, agentCurr)
+        if(movement == 0):
+            #We have moved north
+            world.map[oldX][oldY].qNorth = (1-alpha)*(world.map[oldX][oldY].qNorth) + (alpha)*(reward + (gamma)*nextBestQ)
+        elif(movement == 1):
+            # We have moved east
+            world.map[oldX][oldY].qEast = (1 - alpha) * (world.map[oldX][oldY].qNorth) + (alpha) * (reward + (gamma) * nextBestQ)
+        elif (movement == 2):
+            # We have moved east
+            world.map[oldX][oldY].qSouth = (1 - alpha) * (world.map[oldX][oldY].qNorth) + (alpha) * (reward + (gamma) * nextBestQ)
+        elif (movement == 3):
+            # We have moved east
+            world.map[oldX][oldY].qWest = (1 - alpha) * (world.map[oldX][oldY].qNorth) + (alpha) * (reward + (gamma) * nextBestQ)
 
-    #rates
-    gamma = 1.0;
-    alpha = 0;
 
-    #Previous location of the agent
-    previ = agent.previ;
-    prevj = agent.prevj;
-
-    #Current location of the agent
-    curri = previ;
-    currj = prevj;
-
-    #Direction the agent took
-    q = 0;
-    action = agent.direction
-    if action == "North":
-        q = 0;
-        currj = prevj+1;
-    if action == "East":
-        q = 1;
-        curri = previ+1;
-    if action == "South":
-        q = 2;
-        currj = prevj-1;
-    if action == "West":
-        q = 3;
-        curri = previ-1;
-
-    #Update
-    if (agent.ispickup == True):
-        Qpickupordropoff(previ, prevj, q, curri, currj, alpha, gamma);
-        SARSApickupordropoff(previ, prevj, q, curri, currj, alpha, gamma);
-    else:
-        Qmovespace(previ, prevj, q, curri, currj, alpha, gamma);
-        SARSAmovespace(previ, prevj, q, curri, currj, alpha, gamma);
-
-    #Send Q-Matrix back to file
 
 
 
