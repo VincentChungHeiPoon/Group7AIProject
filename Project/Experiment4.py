f74c6f69e1ba858f897db250429abaef61722ef5
ArgusSanguine <38055179+ArgusSanguine@users.noreply.github.com>
2019-04-13 21:21:40 -0500

    Experiment4 Works
    Experiment4 now works

--- Project/Experiment4.py
+++ Project/Experiment4.py
@@ -3,38 +3,57 @@ from PDWorld import World
 from SelectMove import SelectMove
 from Storing import updateMatrix
 import copy
+import pygame
 from Visualize import Visual
 
 class E4:
 
     agent = Agent(0, 4, False)
-    oldAgent = copy.deepcopy(agent)
+    oldAgent1 = copy.deepcopy(agent)
+    oldAgent2 = copy.deepcopy(agent)
     havePackageWorld = World()
     noPackageWorld = World()
-
+    show = Visual()
+    
     for i in range(200):
-        oldAgent2 = copy.deepcopy(oldAgent)
+        oldAgent2 = copy.deepcopy(oldAgent1)
         oldAgent1 = copy.deepcopy(agent)
         if not (agent.havePackage):
             world = noPackageWorld
+            world.worldUpdate(havePackageWorld, noPackageWorld)
         else:
             world = havePackageWorld
+            world.worldUpdate(noPackageWorld, havePackageWorld)
 
         SelectMove.PRANDOM(agent, world, False)
         newAgent = copy.deepcopy(agent)
-        updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, agent,  world, .3, 1)
-
+        if(i >= 1):
+            if not (oldAgent2.havePackage):
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, noPackageWorld, .3, 1)
+            elif (oldAgent2.havePackage):
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, havePackageWorld, .3, 1)
+    show.run_visual(noPackageWorld, havePackageWorld, agent)
+    
     for j in range(7800):
-        oldAgent = copy.deepcopy(agent)
+        oldAgent2 = copy.deepcopy(oldAgent1)
+        oldAgent1 = copy.deepcopy(agent)
         if not (agent.havePackage):
             world = noPackageWorld
+            world.worldUpdate(havePackageWorld, noPackageWorld)
         else:
             world = havePackageWorld
+            world.worldUpdate(noPackageWorld, havePackageWorld)
 
         SelectMove.PEPLOIT(agent, world, False)
         newAgent = copy.deepcopy(agent)
-        updateMatrix.SARSAUpdate(oldAgent, newAgent, world, .3, 1)
+        if(j >= 1):
+            if not (oldAgent2.havePackage):
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, noPackageWorld, .3, 1)
+            elif (oldAgent2.havePackage):
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, havePackageWorld, .3, 1)
 
-    show = Visual()
+        if j == 1799 or j == 3799 or j == 5799:
+            show.run_visual(noPackageWorld, havePackageWorld, agent)
+
     show.run_visual(noPackageWorld, havePackageWorld, agent)
     show.quit()
