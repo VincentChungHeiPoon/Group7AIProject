9e37e2034ef7e32c95e77208e2d3eb096919053e
JoeBellaish <Joebellaish@gmail.com>
2019-04-13 21:26:21 -0500

    Updated Gamma
    Updated values

--- Project/Experiment3.py
+++ Project/Experiment3.py
@@ -30,7 +30,7 @@ class E3:
 
         SelectMove.PRANDOM(agent, world, False)
         newAgent = copy.deepcopy(agent)
-        updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, world, 0.3, 1)
+        updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, world, 0.3, .5)
         if(world.isCompleteDelevery()):
             noPackageWorld.mapReset()
             havePackageWorld.mapReset()
@@ -58,9 +58,9 @@ class E3:
 
         if (j >= 1):
             if not (oldAgent2.havePackage):
-                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, noPackageWorld, .3, 1)
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, noPackageWorld, .3, .5)
             elif (oldAgent2.havePackage):
-                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, havePackageWorld, .3, 1)
+                updateMatrix.SARSAUpdate(oldAgent2, oldAgent1, newAgent, havePackageWorld, .3, .5)
 
 
         if (world.isCompleteDelevery()):
